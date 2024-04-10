fb0a239b0fb8e34de831462071eab87868fe5d43
YOUNKAP <nina.younkap@ccaa.pro>
2024-04-09 20:05:57 -0400

    refactor 0-appy

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+""" Basic Flask app """
 from flask import Flask, render_template, request
 
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     return render_template('0-index.html')
